6b6d8bf75f64412b62df843bf4a4858d2ae755b3
retrostar1 <balde.deo@gmail.com>
2019-06-09 23:20:54 +0300

    4 homework

---
+++ javascript/js_home/4_homework/doc.js
@@ -0,0 +1,22 @@
+let str = "538754089", sum = 0;
+for (let i = 0; i < str.length; i++)
+sum += parseInt(str[i]);
+console.log(sum);
+
+let str2 = "я читаю книжки брэдберри раз в год", symb = "и", emptyStr = str2;
+for (i=0; i < str2.length; i++) {
+    if (str2.charCodeAt(i) === symb.charCodeAt())
+    emptyStr = emptyStr.slice(0 , i) + symb + emptyStr.slice(i);
+}
+console.log(emptyStr) //не могу найти, в чем загвоздка
+
+let password = prompt('введите пароль');
+reg = /[[a-z][A-Z][^$.|?*+().][0-9]{2}]{9,}/g;
+if (reg.test(password)){
+    alert("Пароль введен верно!");
+} else {
+    alert("Вы неправильно ввели пароль!");
+}
+
+
+
