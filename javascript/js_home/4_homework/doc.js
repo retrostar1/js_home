7508aae5b351b311e1f7653ad107bd0231424054
retrostar1 <balde.deo@gmail.com>
2019-06-13 16:48:47 +0300

    fix 4 homework(исправлено 2 задание)

--- javascript/js_home/4_homework/doc.js
+++ javascript/js_home/4_homework/doc.js
@@ -3,12 +3,14 @@ for (let i = 0; i < str.length; i++)
 sum += parseInt(str[i]);
 console.log(sum);
 
-let str2 = "я читаю книжки брэдберри раз в год", symb = "и", emptyStr = str2;
-for (i=0; i < str2.length; i++) {
-    if (str2.charCodeAt(i) === symb.charCodeAt())
-    emptyStr = emptyStr.slice(0 , i) + symb + emptyStr.slice(i);
+let str2 = "я читаю книжки брэдберри раз в год", symb = "и", emptyStr = "";
+for (let i=0; i < str2.length; i++) {
+    emptyStr += str2[i]
+    if (str2.charCodeAt(i) === symb.charCodeAt()){
+    emptyStr += symb
+    }
 }
-console.log(emptyStr) //не могу найти, в чем загвоздка
+console.log(emptyStr)
 
 let password = prompt('введите пароль');
 reg = /[[a-z][A-Z][^$.|?*+().][0-9]{2}]{9,}/g;
